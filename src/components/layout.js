557ebde76c8d881325acd76655f0729cb6e5214e
Roman-Octavian <work.octavian@gmail.com>
2023-03-31 05:24:55 +0200

    forgot to center content

--- src/components/layout.js
+++ src/components/layout.js
@@ -122,7 +122,7 @@ function Layout(props) {
   const container = window !== undefined ? () => window().document.body : undefined;
 
   return (
-    <Box sx={{ display: 'flex' }}>
+    <Box sx={{ display: 'flex', justifyContent: 'center' }}>
       <AppBar
         position="fixed"
         sx={{
@@ -195,7 +195,11 @@ function Layout(props) {
 
       <Box
         component="main"
-        sx={{ flexGrow: 1, p: 3, width: { sm: `calc(100% - ${drawerWidth}px)` } }}
+        sx={{ 
+          flexGrow: 1, p: 3, 
+          maxWidth: 'md',
+          justifyContent: 'center', 
+          width: { sm: `calc(100% - ${drawerWidth}px)` } }}
       >
       <Toolbar />
       {props.content}
